8cb3840bb413703fa6ee5b045c220f63c7d1e331
gmbnt <andriesseniels@gmail.com>
2020-04-03 21:14:42 +1100

    Add TODO

--- SessionCore/Utilities/Dictionary+Description.swift
+++ SessionCore/Utilities/Dictionary+Description.swift
@@ -4,7 +4,7 @@ internal extension Dictionary {
     var prettifiedDescription: String {
         return "[ " + map { key, value in
             let keyDescription = String(describing: key)
-            let valueDescription = String(describing: value) // TODO: Call prettifiedDescription on value if it's an array of CustomStringConvertibles
+            let valueDescription = String(describing: value) // TODO: Call prettifiedDescription on value if it's an array of CustomStringConvertibles or a dictionary
             let maxLength = 40
             let truncatedValueDescription = valueDescription.count > maxLength ? valueDescription.prefix(maxLength) + "..." : valueDescription
             return keyDescription + " : " + truncatedValueDescription
