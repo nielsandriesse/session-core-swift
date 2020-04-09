18ad12bee9f27a1247bad53bbfdd8cfcf0de0980
nielsandriesse <andriesseniels@gmail.com>
2020-04-09 17:38:44 +1000

    Minor refactoring

--- SessionCore/Message+PoW.swift
+++ SessionCore/Message+PoW.swift
@@ -1,18 +1,14 @@
 import CryptoSwift
 import PromiseKit
 
-// TODO: Clean
-
 public extension Message {
 
-    // MARK: Settings
-    private static let nonceSize = 8
-
     // MARK: Proof of Work Calculation
-    /// Based on [Bitmessage's Proof of Work Implementation](https://bitmessage.org/wiki/Proof_of_work).
+    /// A modified version of [Bitmessage's Proof of Work Implementation](https://bitmessage.org/wiki/Proof_of_work).
     ///
     /// - Note: Exposed for testing purposes.
-    static func calculatePow(ttl: UInt64, destination: String, data: String) -> (timestamp: UInt64, base64EncodedNonce: String) {
+    static func calculatePoW(ttl: UInt64, destination: String, data: String) -> (timestamp: UInt64, base64EncodedNonce: String)? {
+        let nonceSize = 8
         // Get millisecond timestamp
         let timestamp = UInt64(Date().timeIntervalSince1970 * 1000) // timeIntervalSince1970 has millisecond level precision
         // Construct payload
@@ -21,18 +17,19 @@ public extension Message {
         // Calculate target
         let numerator = UInt64.max
         let difficulty = UInt64(SnodeAPI.powDifficulty)
-        let totalSize = UInt64(payload.count + Message.nonceSize)
+        let totalSize = UInt64(payload.count + nonceSize)
         let ttlInSeconds = ttl / 1000
         let denominator = difficulty * (totalSize + (ttlInSeconds * totalSize) / UInt64(UInt16.max))
         let target = numerator / denominator
         // Calculate proof of work
-        var currentTrialValue = UInt64.max
-        let initialHash = payload.sha512()
-        var nonce = [Byte](repeating: 0, count: Message.nonceSize)
-        while currentTrialValue > target {
-            nonce = nonce.increment(by: 1)
-            let newHash = (nonce + initialHash).sha512()
-            currentTrialValue = UInt64([Byte](newHash[0..<8]))
+        var currentValue = UInt64.max
+        let payloadHash = payload.sha512()
+        var nonce = [Byte](repeating: 0, count: nonceSize)
+        while currentValue > target {
+            nonce = nonce.adding(1)
+            let hash = (nonce + payloadHash).sha512()
+            guard let value = UInt64([Byte](hash[0..<nonceSize])) else { return nil }
+            currentValue = value
         }
         // Encode as base 64
         let base64EncodedNonce = nonce.toBase64()!
@@ -45,7 +42,7 @@ public extension Message {
         var copy = self
         return Promise<Message> { seal in
             DispatchQueue.global().async {
-                let (timestamp, base64EncodedNonce) = Message.calculatePow(ttl: copy.ttl, destination: copy.destination, data: copy.data)
+                guard let (timestamp, base64EncodedNonce) = Message.calculatePoW(ttl: copy.ttl, destination: copy.destination, data: copy.data) else { return seal.reject(SnodeAPI.Error.proofOfWorkCalculationFailed) }
                 copy.timestamp = timestamp
                 copy.nonce = base64EncodedNonce
                 seal.fulfill(copy)
@@ -54,32 +51,33 @@ public extension Message {
     }
 }
 
+// MARK: Convenience
 private typealias Byte = UInt8
+
+private extension UInt64 {
+
+    /// Assumes `bytes` is big endian.
+    init?(_ bytes: [Byte]) {
+        guard bytes.count <= MemoryLayout<UInt64>.size else { return nil }
+        self = bytes.reduce(0) { ($0 << 8) | UInt64($1) }
+    }
+}
 
 private extension MutableCollection where Element == Byte {
 
-    func increment(by amount: Int) -> Self {
+    /// Assumes `self` represents a big endian number.
+    ///
+    /// - Note: Can overflow.
+    func adding(_ amount: Int) -> Self {
         var result = self
         var amountRemaining = amount
         for index in result.indices.reversed() {
             guard amountRemaining > 0 else { break }
             let sum = Int(result[index]) + amountRemaining
-            result[index] = Byte(sum % 256)
-            amountRemaining = sum / 256
+            let (quotient, remainder) = sum.quotientAndRemainder(dividingBy: 256)
+            result[index] = Byte(remainder)
+            amountRemaining = quotient
         }
         return result
-    }
-}
-
-private extension UInt64 {
-
-    init(_ bytes: [Byte]) {
-        precondition(bytes.count <= MemoryLayout<UInt64>.size)
-        self = bytes.reduce(0) { current, byte in
-            var new = current
-            new = new << 8
-            new = new | UInt64(byte)
-            return new
-        }
     }
 }
